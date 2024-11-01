d3b19065a445879ca72306f01303e5d0eeed8552
MrSohailAhmad <ahmadsohail1819@gmail.com>
2024-11-01 22:19:41 +0500

    view fixes

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -162,7 +162,7 @@ const HeroSection = ({ heroRef }) => {
   return (
     <div
       ref={heroRef}
-      className="mx-auto w-full flex h-[140vh] sm:h-[150vh] md:h-[160vh] lg:h-[80vh] gap-10 items-center flex-col lg:flex-row justify-center max-w-screen-xl m-4 "
+      className="mx-auto w-full hero-sec flex h-[140vh] sm:h-[150vh] md:h-[160vh] lg:h-[80vh] gap-10 items-center flex-col lg:flex-row justify-center max-w-screen-xl m-4 "
     >
       <motion.div
         ref={ref}
@@ -178,16 +178,16 @@ const HeroSection = ({ heroRef }) => {
       </motion.div>
       <div className="w-[90%] md:w-[80%] lg:w-[50%] h-full lg:h-full flex items-start  gap-3 flex-col">
         <div className="flex flex-col gap-3">
-          <span className="w-full md:text-xl text-[1.2rem]  lg:text-3xl font-bold">
+          <span className="w-full sm:text-xl  md:text-[1rem] text-[1rem]  lg:text-2xl font-bold">
             Family-Oriented 3 & 4BR Villas{" "}
           </span>
-          <span className="  md:text-2xl text-[1.2rem] lg:text-2xl font-[500]">
+          <span className="  md:text-xl text-[1.2rem] lg:text-xl font-[500]">
             in AVENA AT THE VALLEY by EMAAR, Dubai
           </span>
         </div>
         <div className="p-1 w-[40%] bg-black" />
         <div>
-          <p className="font-light hero-text  md:text-md text-sm text-md lg:text-xl">
+          <p className="font-light hero-text   lg:text-lg">
             Avena at The Valley is a place where modern living meets the gentle
             touch of nature. This charming community of elegant 4-bedroom villas
             is designed to offer a sustainable lifestyle, nestled in harmony
